26a930a38bbe2cd43617f2afe6326b1d09aa60b8
Zenjutahi <zenjutahi@gmail.com>
2019-07-17 14:32:45 +0300

    [chore] - debug json_web_token file

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -1,8 +1,8 @@
 class JsonWebToken 
   # secret to encode and decode token 
-  ZMAC_SECRET = Rails.application.secrets.secret_key_base
+  ZMAC_SECRET = TodosApi::Application.credentials.secret_key_base
   
-  def self.encode(payload, exp = 24.hours.from.now)
+  def self.encode(payload, exp = 24.hours.from_now)
     # set expiry to 24 hours from creation time
     payload[:exp] = exp.to_i
     # sign token with appliction secret
@@ -12,7 +12,7 @@ class JsonWebToken
   def self.decode(token)
     # get payload; first index in decoded array
     body = JWT.decode(token, ZMAC_SECRET)[0]
-    HashWishIndifferentAccess.new body
+    HashWithIndifferentAccess.new body
     # rescue from all decode errors
   rescue JWT::DecodeError => e
     # raise custom error to be handled by custom handler
