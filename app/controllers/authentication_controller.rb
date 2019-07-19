ecc9a68a4bc28af665a3fd5b0013e31efc819f69
Zenjutahi <zenjutahi@gmail.com>
2019-07-19 10:27:33 +0300

    [feature] - add authentication

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,10 +1,8 @@
 class AuthenticationController < ApplicationController
   # return auth token once user is authenticated
   def authenticate
-    auth_token = AuthenticateUser.new(
-      auth_params[:email],
-      auth_params[:password]
-    ).call
+    auth_token = 
+      AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
     json_response(auth_token: auth_token)
   end
   
